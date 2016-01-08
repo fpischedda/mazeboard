d01956f899e505de123dd7081ea81f767e6e02cc
(EXT) Francesco Pischedda <francesco.pischedda@gmail.com>
2016-01-08 14:57:48 +0100

    added global fixture to be used in the tests

--- test/mazeboard/tile_test.clj
+++ test/mazeboard/tile_test.clj
@@ -2,18 +2,20 @@
   (:use midje.sweet)
   (:require [mazeboard.tile :as tile]))
 
+(def the-tile (tile/make-tile :solid :solid :open :open))
+
 (facts "Tile is an array of four elements that can be :solid or :open"
        (fact "Tiles is composed of four elements"
              (tile/make-tile :solid :solid :solid :solid) => [:solid :solid :solid :solid])
        (fact "I can get the tile wall at :north :east :south :west"
-             (tile/wall-at :north (tile/make-tile :solid :open :solid :open)) => :solid
-             (tile/wall-at :east (tile/make-tile :solid :open :solid :open)) => :open
-             (tile/wall-at :south (tile/make-tile :solid :open :solid :open)) => :solid
-             (tile/wall-at :west (tile/make-tile :solid :open :solid :open)) => :open)
+             (tile/wall-at :north the-tile) => :solid
+             (tile/wall-at :east the-tile) => :solid
+             (tile/wall-at :south the-tile) => :open
+             (tile/wall-at :west the-tile) => :open)
        (fact "Tiles can be rotated clockwise or counterclockwise"
-             (tile/rotate-tile-left (tile/make-tile :solid :solid :open :open)) => [:solid :open :open :solid]
-             (tile/rotate-tile-right (tile/make-tile :solid :solid :open :open)) => [:open :solid :solid :open]
-             (tile/rotate-tile :left (tile/make-tile :solid :solid :open :open)) => [:solid :open :open :solid]
-             (tile/rotate-tile :right (tile/make-tile :solid :solid :open :open)) => [:open :solid :solid :open]
+             (tile/rotate-tile-left the-tile) => [:solid :open :open :solid]
+             (tile/rotate-tile-right the-tile) => [:open :solid :solid :open]
+             (tile/rotate-tile :left the-tile) => [:solid :open :open :solid]
+             (tile/rotate-tile :right the-tile) => [:open :solid :solid :open]
              )
        )
