cdbbdf32630852768f96cb9f0ea635b1be787cea
e_fpischedda <francesco.pischedda@abinsula.com>
2016-11-08 15:43:15 +0100

    fake create/destroy server functions just to compile settings.clj

--- src/mazeboard/system.clj
+++ src/mazeboard/system.clj
@@ -1,6 +1,18 @@
 (ns mazeboard.system
   (require [mount.core :refer [defstate]]))
 
+(defn create-web-server
+  "creates a web serverv"
+  []
+  ())
+
+(defn destroy-web-server
+  "destroy the web serverv"
+  [server]
+  ())
+
+(defn destroy-web-server [] ())
+
 (defstate web-server
   :start (create-web-server)
   :stop (destroy-web-server web-server))
