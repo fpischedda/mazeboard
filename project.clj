88feb4f0c19040b19d13d60c3b4f6ffcc566b304
e_fpischedda <francesco.pischedda@abinsula.com>
2017-01-02 10:27:25 +0100

    moved common things in src/common, project.clj contains errors

--- project.clj
+++ project.clj
@@ -10,9 +10,68 @@
                  [ring/ring-core "1.2.1"]
                  ]
   :main ^:skip-aot mazeboard.core
-  :plugins [[lein-ring "0.8.11"] [lein-midje "3.2"]]
+  :plugins [[lein-ring "0.8.11"]
+            [lein-midje "3.2"]
+            [lein-cljsbuild "1.1.1"]
+            [lein-figwheel "0.5.0-3"]]
   :ring {:handler mazeboard.api/handler}
   :target-path "target/%s"
   :test-paths ["test" "test/mazeboard"]
   :profiles {:uberjar {:aot :all}
              :dev {:dependencies [[midje "1.8.3"]]}})
+
+  :source-paths ["src/cljs" "src/common" "src/clj"]
+
+  :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
+
+  :cljsbuild {:builds
+              [{:id "dev"
+                :source-paths ["src/cljs" "src/common"]
+
+                :figwheel {:on-jsload "mazeboardui.core/on-js-reload"}
+
+                :compiler {:main mazeboardui.core
+                           :asset-path "js/compiled/out"
+                           :output-to "resources/public/js/compiled/mazeboardui.js"
+                           :output-dir "resources/public/js/compiled/out"
+                           :source-map-timestamp true}}
+               ;; This next build is an compressed minified build for
+               ;; production. You can build this with:
+               ;; lein cljsbuild once min
+               {:id "min"
+                :source-paths ["src/cljs" "src/common"]
+                :compiler {:output-to "resources/public/js/compiled/mazeboardui.js"
+                           :main mazeboardui.core
+                           :optimizations :advanced
+                           :pretty-print false}}]}
+
+  :figwheel {;; :http-server-root "public" ;; default and assumes "resources"
+             ;; :server-port 3449 ;; default
+             ;; :server-ip "127.0.0.1"
+
+             :css-dirs ["resources/public/css"] ;; watch and update CSS
+
+             ;; Start an nREPL server into the running figwheel process
+             ;; :nrepl-port 7888
+
+             ;; Server Ring Handler (optional)
+             ;; if you want to embed a ring handler into the figwheel http-kit
+             ;; server, this is for simple ring servers, if this
+             ;; doesn't work for you just run your own server :)
+             ;; :ring-handler hello_world.server/handler
+
+             ;; To be able to open files in your editor from the heads up display
+             ;; you will need to put a script on your path.
+             ;; that script will have to take a file path and a line number
+             ;; ie. in  ~/bin/myfile-opener
+             ;; #! /bin/sh
+             ;; emacsclient -n +$2 $1
+             ;;
+             ;; :open-file-command "myfile-opener"
+
+             ;; if you want to disable the REPL
+             ;; :repl false
+
+             ;; to configure a different figwheel logfile path
+             ;; :server-logfile "tmp/logs/figwheel-logfile.log"
+             })
