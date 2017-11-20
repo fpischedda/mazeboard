03c8d2eab51a02e8833e33fabb1c8f6edaedf02a
francesco.pischedda <francesco.pischedda@fincompare.com>
2017-11-20 15:20:56 +0100

    properly stopping api server

--- src/clj/mazeboard/api/server.clj
+++ src/clj/mazeboard/api/server.clj
@@ -3,7 +3,30 @@
             [mazeboard.config :refer [config]]
             [mount.core :refer [defstate]]
             [compojure.handler :refer [site]]
+            [buddy.auth.backends :as backends]
+            [buddy.auth.middleware :refer (wrap-authentication)]
             [org.httpkit.server :refer [run-server]]))
 
+(defonce server-instance (atom nil))
+
+(def auth-backend (backends/jws {:secret (:auth-secret config)}))
+
+(def app (-> (site #'routes)
+             (wrap-authentication auth-backend)))
+
+(defn start []
+  (let [api-server (:api-server config)]
+    (println "starting api server " api-server)
+    (reset! server-instance (run-server app api-server))))
+
+(defn stop []
+  (when-not (nil? @server-instance)
+    (println "quitting api server")
+    ;; graceful shutdown: wait 100ms for existing requests to be finished
+    ;; :timeout is optional, when no timeout, stop immediately
+    (@server-instance :timeout 100)
+    (reset! server-instance nil)))
+
 (defstate server
-  :start (run-server (site #'routes) (:server config)))
+  :start (start)
+  :stop (stop))
