4556c903a4a8bb8502cc7691cc9c87259d2fcca8
francesco.pischedda <francesco.pischedda@fincompare.com>
2017-11-21 12:11:54 +0100

    use uuid instead of mongo ObjectId

--- src/clj/mazeboard/data/games.clj
+++ src/clj/mazeboard/data/games.clj
@@ -1,36 +1,36 @@
 (ns mazeboard.data.games
   (:require [monger.collection :as mc]
             [monger.operators :refer :all]
-            [mazeboard.data.utils :refer [paged-filter]]
-            [mazeboard.data.connection :refer [database]])
-  (:import org.bson.types.ObjectId))
+            [mazeboard.data.utils :refer [paged-filter gen-id]]
+            [mazeboard.data.connection :refer [database]]))
 
 (defn create [user-id max-players]
-  (mc/insert-and-return database "games" {:created-by user-id
+  (mc/insert-and-return database "games" {:_id (gen-id)
+                                          :created-by user-id
                                           :max-players max-players
                                           :players [user-id]
                                           :players-size 1
                                           :status :created}))
 (defn details [game-id]
-  (mc/find-one-as-map database "games" {:_id (ObjectId. game-id)}))
+  (mc/find-one-as-map database "games" {:_id game-id}))
 
 (defn by-user [user-id page page-size]
   (paged-filter database "games" page page-size {:players {$in [user-id]}}))
 
 (defn join [game-id user-id]
   (mc/update database "games"
-             {:_id (ObjectId. game-id) :players-size {$lt :max-players}
+             {:_id game-id :players-size {$lt :max-players}
               :players {$nin [user-id]} :status :created}
              {$addToSet {:players user-id} $inc {:players-size 1}}))
 
 (defn leave [game-id user-id]
   (mc/update database "games"
-             {:_id (ObjectId. game-id)
+             {:_id game-id
               :players {$in [user-id]} :status :created}
              {$pull {:players user-id} $inc {:players-size -1}}))
 
 (defn start [game-id board]
   (mc/update database "games"
-             {:_id (ObjectId. game-id)
+             {:_id game-id
               :players-size :max-players :status :created}
              {$set {:status :running :board-history [board]}}))
