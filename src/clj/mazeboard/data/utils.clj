dcfe54e96f950b327e98943f445c79d865fcfcc4
francesco.pischedda <francesco.pischedda@fincompare.com>
2017-11-21 12:11:21 +0100

    gen-id function based on uuid/v1

--- src/clj/mazeboard/data/utils.clj
+++ src/clj/mazeboard/data/utils.clj
@@ -1,6 +1,7 @@
 (ns mazeboard.data.utils
   (:require [monger.collection :as mc]
             [monger.query :as mq]
+            [clj-uuid :as uuid]
             [mazeboard.data.connection :refer [database]]))
 
 (defn paged-filter [database collection page page-size filters-map]
@@ -8,3 +9,6 @@
     (mc/find-maps filters-map)
     (mq/skip (* page page-size))
     (mq/limit page-size)))
+
+(defn gen-id []
+  (str (uuid/v1)))
