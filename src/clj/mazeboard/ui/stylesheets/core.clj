5c882668ef64f6eb20eba37d3668d4419b54631d
francesco.pischedda <francesco.pischedda@fincompare.com>
2017-11-30 15:25:59 +0100

    small style changes

--- src/clj/mazeboard/ui/stylesheets/core.clj
+++ src/clj/mazeboard/ui/stylesheets/core.clj
@@ -2,6 +2,11 @@
   (:require
    [garden.def :refer [defstyles]]))
 
+  (def default-padding "2px 10px")
+
 (defstyles style
-  [:.profile {:background-color "#eee"}])
+  [:.profile {:background-color "#eee" :padding default-padding}
+   [:div {:display "flex" :justify-content "space-between"}
+    [:.greeting {:text-color "#333"}]
+    [:.logout {}]]])
 
