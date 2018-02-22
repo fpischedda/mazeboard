f4597f2c71750d7ec4ab412c4636fa0a70426873
Francesco Pischedda <francesco.pischedda@vimcar.de>
2018-02-22 15:46:30 +0100

    UI-2 added tile background image

--- src/clj/mazeboard/ui/stylesheets/core.clj
+++ src/clj/mazeboard/ui/stylesheets/core.clj
@@ -1,13 +1,15 @@
 (ns mazeboard.ui.stylesheets.core
   (:require
-   [garden.def :refer [defstyles]]))
+   [garden.def :refer [defstyles defcssfn]]))
+
+(defcssfn url)
 
 (def default-padding "2px 10px")
 (def small-padding "1px 5px")
 (def wall "solid 4px red")
 
 (defstyles style
-  [:body {:background-color "#eee" :text-color "222" :padding default-padding}]
+  [:body {:background-color "#eee" :text-color "222"}]
   [:.hidden {:display :none}]
   [:.profile {:background-color "#ddd" :padding small-padding}
    [:div {:display :flex :justify-content :space-between}
@@ -18,13 +20,14 @@
     [:thead {:background-color "#dddddd"}]
     [:tbody {:background-color "#eaeaea"}]]]
   [:.tile {:float :left
-              :background-color "#333300"
-              :display :table
-              :width "64px"
-              :height "64px"
-              :border "solid 4px black"
-              :margin "0.2em"
-              :color :white}]
+           :background-color "#333300"
+           :background-image (url "/images/tile_1.png")
+           :display :table
+           :width "128px"
+           :height "128px"
+           :border "solid 4px black"
+           :margin "0.2em"
+           :color :white}]
   [:div.board-row {:clear :both}]
   [:dib.board {:margin-left :auto
                :margin-right :auto}]
