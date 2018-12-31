a1264e7a21cfe88c57b090ad6ce277c243a11420
Francesco Pischedda <francesco.pischedda@vimcar.de>
2018-12-31 15:48:44 +0100

    small refactoring

--- src/clj/mazeboard/api/games.clj
+++ src/clj/mazeboard/api/games.clj
@@ -2,7 +2,7 @@
   (:require
    [cheshire.core :as json]
    [compojure.core :refer [context GET POST PATCH DELETE]]
-   [mazeboard.api.response :refer [error bad-request json-success created accepted]]
+   [mazeboard.api.response :as response]
    [mazeboard.data.games :as games]
    [mazeboard.dice :as dice]
    [mazeboard.game :as game-logic]))
@@ -24,23 +24,23 @@
         max-players (Integer. (:max-players params))
         board-size (Integer. (:board-size params))
         game (games/create user max-players board-size :coin)]
-    (created (game-url game))))
+    (response/created (game-url game))))
 
 (defn details [req]
   (let [id (game-id req)]
-    (json-success (games/details id))))
+    (response/json-success (games/details id))))
 
 (defn join [req]
   (let [user (get-user req)
         id (game-id req)]
     (games/join id user)
-    (accepted)))
+    (response/accepted)))
 
 (defn leave [req]
   (let [user (get-user req)
         id (game-id req)]
     (games/leave id user)
-    (accepted)))
+    (response/no-content)))
 
 (defn start [req]
   (let [user (get-user req)
@@ -53,22 +53,22 @@
     (when game
       (let [res (games/start id user board move)]
         (if (= (:res res) :ok)
-          (json-success (games/current-turn id))
-          (bad-request res))))))
+          (response/json-success (games/current-turn id))
+          (response/bad-request res))))))
 
 (defn user-games [req]
   (let [user (get-user req)]
-    (json/encode (games/by-user user 0 10))))
+    (response/json-success (games/by-user user 0 10))))
 
 (defn update-max-players [req]
   (let [id (game-id req)
         user (get-user req)
         max-players (Integer. (:max-players (:params req)))]
-    (json-success (games/update-max-players id user max-players))))
+    (response/json-success (games/update-max-players id user max-players))))
 
 (defn current-turn [req]
   "returns the current turn with options"
-  (json-success (games/current-turn (game-id req))))
+  (response/json-success (games/current-turn (game-id req))))
 
 (defn apply-turn [req]
   (let [id (game-id req)
@@ -76,23 +76,24 @@
         move (:move (:params req))
         turn (games/current-turn id)]
     (if-let [errors (game-logic/validate-move-format turn move)]
-      (error errors)
-      (json-success (game-logic/handle-turn turn move user)))))
+      (response/error errors)
+      (response/json-success (game-logic/handle-turn turn move user)))))
 
 (defn abandon-game [req]
   (let [id (game-id req)
         user (get-user req)]
-    (json/encode (games/close id user))))
+    (response/no-content)))
 
-(def routes (context "/games" []
-                     (GET "/" [] user-games)
-                     (POST "/" [] create)
-                     (context "/:id" [id]
-                              (GET "/" [] details)
-                              (PATCH "/" [] update-max-players)
-                              (GET "/current-turn" [] current-turn)
-                              (POST "/turn" [] apply-turn)
-                              (POST "/join" [] join)
-                              (POST "/leave" [] leave)
-                              (POST "/start" [] start)
-                              (DELETE "/" [] abandon-game))))
+(def routes
+  (context "/games" []
+    (GET "/" [] user-games)
+    (POST "/" [] create)
+    (context "/:id" [id]
+      (GET "/" [] details)
+      (PATCH "/" [] update-max-players)
+      (GET "/current-turn" [] current-turn)
+      (POST "/turn" [] apply-turn)
+      (POST "/join" [] join)
+      (POST "/leave" [] leave)
+      (POST "/start" [] start)
+      (DELETE "/" [] abandon-game))))
